f3f92cf486977c32d39090a93df7a3f43876fbed
amorist <amor90s.zhang@gmail.com>
2020-08-03 19:50:43 +0800

    Update

--- open/video/video.go
+++ open/video/video.go
@@ -21,6 +21,10 @@ const (
 	videoCreateURL string = "https://open.douyin.com/video/create?access_token=%s&open_id=%s"
 	// 删除视频
 	videoDeleteURL string = "https://open.douyin.com/video/delete?access_token=%s&open_id=%s"
+
+	// 视频列表
+	videoListURL string = "https://open.douyin.com/video/list?access_token=%s&open_id=%s&cursor=%d&count=%d"
+	videoDataURL string = "https://open.douyin.com/video/data?access_token=%s&open_id=%s"
 )
 
 // Video 视频
@@ -179,6 +183,7 @@ type CreateVideoReq struct {
 	CoverTsp          float64  `json:"cover_tsp"`
 	GameID            string   `json:"string"`
 	PoiID             string   `json:"poi_id"`
+	PoiName           string   `json:"poi_name"`
 	Text              string   `json:"text"`
 	MicroAppURL       string   `json:"micro_app_url"`
 	MicroAppID        string   `json:"micro_app_id"`
@@ -242,7 +247,7 @@ type deleteVideoRes struct {
 
 // Delete 视频删除
 // refer: https://open.douyin.com/platform/doc/6848806536383383560#url
-func (video *Video) Delete(openid, itemid string) {
+func (video *Video) Delete(openid, itemid string) (err error) {
 	accessToken, err := video.GetAccessToken(openid)
 	if err != nil {
 		return
@@ -269,5 +274,124 @@ func (video *Video) Delete(openid, itemid string) {
 		err = fmt.Errorf("Delete error : errcode=%v , errmsg=%v", result.Data.ErrCode, result.Data.ErrMsg)
 		return
 	}
+	return
+}
+
+// ListInfo video list info.
+type ListInfo struct {
+	util.CommonError
+
+	Total   int64 `json:"total"`
+	Cursor  int64 `json:"cursor"`
+	HasMore bool  `json:"has_more"`
+	List    []struct {
+		Statistics struct {
+			CommentCount  int32 `json:"comment_count"`
+			DiggCount     int32 `json:"digg_count"`
+			DownloadCount int32 `json:"download_count"`
+			ForwardCount  int32 `json:"forward_count"`
+			PlayCount     int32 `json:"play_count"`
+			ShareCount    int32 `json:"share_count"`
+		} `json:"statistics"`
+		Title      string `json:"title"`
+		Cover      string `json:"cover"`
+		CreateTime int64  `json:"create_time"`
+		IsReviewed bool   `json:"is_reviewed"`
+		IsTop      bool   `json:"is_top"`
+		ItemID     string `json:"item_id"`
+		ShareURL   string `json:"share_url"`
+	} `json:"list"`
+}
+
+type listInfoRes struct {
+	Message string   `json:"message"`
+	Data    ListInfo `json:"data"`
+}
+
+// List .
+func (video *Video) List(openid string, cursor, count int64) (info *ListInfo, err error) {
+	accessToken, err := video.GetAccessToken(openid)
+	if err != nil {
+		return
+	}
+	uri := fmt.Sprintf(videoListURL, accessToken, openid, cursor, count)
+	var response []byte
+	response, err = util.HTTPGet(uri)
+	if err != nil {
+		return
+	}
+	var result listInfoRes
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+	if result.Data.ErrCode != 0 {
+		err = fmt.Errorf("List error : errcode=%v , errmsg=%v", result.Data.ErrCode, result.Data.ErrMsg)
+		return
+	}
+	info = &result.Data
+	return
+}
+
+// DataInfo video data info.
+type DataInfo struct {
+	util.CommonError
+
+	List []struct {
+		Statistics struct {
+			CommentCount  int32 `json:"comment_count"`
+			DiggCount     int32 `json:"digg_count"`
+			DownloadCount int32 `json:"download_count"`
+			ForwardCount  int32 `json:"forward_count"`
+			PlayCount     int32 `json:"play_count"`
+			ShareCount    int32 `json:"share_count"`
+		} `json:"statistics"`
+		Title      string `json:"title"`
+		Cover      string `json:"cover"`
+		CreateTime int64  `json:"create_time"`
+		IsReviewed bool   `json:"is_reviewed"`
+		IsTop      bool   `json:"is_top"`
+		ItemID     string `json:"item_id"`
+		ShareURL   string `json:"share_url"`
+	} `json:"list"`
+}
+
+// DataReq .
+type DataReq struct {
+	ItemIDS []string `json:"item_ids"`
+}
+
+type dataInfoRes struct {
+	Message string   `json:"message"`
+	Data    DataInfo `json:"data"`
+}
+
+// Data .
+func (video *Video) Data(openid string, itemIDS []string) (info *DataInfo, err error) {
+	accessToken, err := video.GetAccessToken(openid)
+	if err != nil {
+		return
+	}
+
+	uri := fmt.Sprintf(videoDataURL, accessToken, openid)
+	req := &DataReq{
+		ItemIDS: itemIDS,
+	}
+
+	var response []byte
+	response, err = util.PostJSON(uri, req)
+	if err != nil {
+		return
+	}
+	var result dataInfoRes
+	err = json.Unmarshal(response, &result)
+	if err != nil {
+		return
+	}
+	if result.Data.ErrCode != 0 {
+		err = fmt.Errorf("Data error : errcode=%v , errmsg=%v", result.Data.ErrCode, result.Data.ErrMsg)
+		return
+	}
+	info = &result.Data
 	return
 }
